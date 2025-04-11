b3fbf71b1edf5fb8df9a781cd3f7a959a891721d
DanielePellati <65215381+DanielePellati@users.noreply.github.com>
2025-04-11 19:47:37 +0200

    Create index.php

---
+++ WWW/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="/jquery/jquery-3.7.1.min.js"></script>
+    <script src="/front_end/index.js"></script>
+</head>
+<body>
+    <table id = "scelta_tassonomia">
+        <tr>
+            <th>Nome Tassonomia</th>
+            <th>Visualizza</th> 
+        </tr>        
+    </table>
+</body>
+
+
+<style>
+    table, th, td {
+        border: 1px solid black;
+    }
+</style>
